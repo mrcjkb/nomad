1a785a04ed8fd3ff5002e5469e0075a72d0e936b
Riccardo Mazzarini <me@noib3.dev>
2024-03-09 13:36:56 +0100

    nomad: implement `MaybeResult`

--- crates/nomad/src/maybe_result.rs
+++ crates/nomad/src/maybe_result.rs
@@ -3,8 +3,32 @@
 use std::error::Error as StdError;
 
 /// TODO: docs
-pub trait MaybeResult<T> {}
+pub trait MaybeResult<T> {
+    /// TODO: docs
+    type Error: StdError;
 
-impl<T> MaybeResult<T> for T {}
+    /// TODO: docs
+    fn into_result(self) -> Result<T, Self::Error>;
+}
 
-impl<T, E> MaybeResult<T> for Result<T, E> where E: StdError {}
+impl<T> MaybeResult<T> for T {
+    // TODO: change this to the never type (!) when it becomes stable.
+    type Error = core::convert::Infallible;
+
+    #[inline]
+    fn into_result(self) -> Result<T, Self::Error> {
+        Ok(self)
+    }
+}
+
+impl<T, E> MaybeResult<T> for Result<T, E>
+where
+    E: StdError,
+{
+    type Error = E;
+
+    #[inline]
+    fn into_result(self) -> Result<T, Self::Error> {
+        self
+    }
+}
