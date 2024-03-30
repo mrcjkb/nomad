a8b76b8a76948e768e27a90592d769ae58d5e51c
Riccardo Mazzarini <me@noib3.dev>
2024-03-30 19:11:11 +0100

    nomad: turn `nvim-oxi`'s `Buffer` into `BufferId`

--- crates/nomad/src/editor/buffer_id.rs
+++ crates/nomad/src/editor/buffer_id.rs
@@ -1,3 +1,10 @@
 /// TODO: docs
 #[derive(Copy, Clone)]
 pub struct BufferId(nvim::BufHandle);
+
+impl From<&nvim::api::Buffer> for BufferId {
+    #[inline]
+    fn from(buf: &nvim::api::Buffer) -> Self {
+        Self(unsafe { core::mem::transmute_copy(buf) })
+    }
+}
