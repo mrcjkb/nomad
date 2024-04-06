1a41314fca20f2b0e4dbe883c3093e83a4287c8b
Riccardo Mazzarini <me@noib3.dev>
2024-04-06 13:27:49 +0200

    clipboard: document it

--- crates/clipboard/src/lib.rs
+++ crates/clipboard/src/lib.rs
@@ -1,5 +1,68 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! TODO: docs
+
+use core::fmt::{self, Debug, Display};
+use std::error::Error as StdError;
+
+use copypasta::{ClipboardContext, ClipboardProvider};
+
+/// TODO: docs
+#[inline]
+pub fn get() -> Result<String, ClipboardError> {
+    ClipboardContext::new()
+        .and_then(|mut ctx| ctx.get_contents())
+        .map_err(ClipboardError::new_get)
+}
+
+/// TODO: docs
+#[inline]
+pub fn set<T: Display>(value: T) -> Result<(), ClipboardError> {
+    ClipboardContext::new()
+        .and_then(|mut ctx| ctx.set_contents(value.to_string()))
+        .map_err(ClipboardError::new_set)
+}
+
+/// TODO: docs
+#[derive(Debug)]
+pub struct ClipboardError {
+    inner: Box<dyn StdError + Send + Sync + 'static>,
+    kind: ClipboardErrorKind,
+}
+
+impl ClipboardError {
+    #[inline]
+    fn new_get(inner: Box<dyn StdError + Send + Sync + 'static>) -> Self {
+        Self { kind: ClipboardErrorKind::Get, inner }
+    }
+
+    #[inline]
+    fn new_set(inner: Box<dyn StdError + Send + Sync + 'static>) -> Self {
+        Self { kind: ClipboardErrorKind::Set, inner }
+    }
+}
+
+impl Display for ClipboardError {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "couldn't {} clipboard: {}", self.kind, self.inner)
+    }
+}
+
+impl StdError for ClipboardError {}
+
+#[derive(Debug, Eq, PartialEq)]
+enum ClipboardErrorKind {
+    Get,
+    Set,
+}
+
+impl Display for ClipboardErrorKind {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Get => write!(f, "get"),
+            Self::Set => write!(f, "set"),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +70,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn clipboard_set_get_cycle() {
+        for idx in 0..10 {
+            set(idx).unwrap();
+            assert_eq!(get().unwrap(), idx.to_string());
+        }
     }
 }
