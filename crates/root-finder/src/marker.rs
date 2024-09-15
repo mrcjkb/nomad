b4bc992c41d228745234d8a5ff62d4153f65d1eb
Riccardo Mazzarini <me@noib3.dev>
2024-09-15 22:33:33 +0200

    root-finder: implement `Marker` for `Git`

--- crates/root-finder/src/marker.rs
+++ crates/root-finder/src/marker.rs
@@ -1,2 +1,15 @@
+use core::future::Future;
+use std::io;
+
+use collab_fs::{AbsUtf8Path, Fs};
+
 /// TODO: docs.
-pub trait Marker {}
+pub trait Marker {
+    /// Whether the marker matches the file or directory at the given path.
+    fn matches<F: Fs>(
+        &self,
+        path: &AbsUtf8Path,
+        metadata: &F::Metadata,
+        fs: &F,
+    ) -> impl Future<Output = io::Result<bool>>;
+}
