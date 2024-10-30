b53b849f32f90bab1e85f88a3ddc9875010060c5
Riccardo Mazzarini <me@noib3.dev>
2024-10-30 15:14:49 +0800

    collab2: add `SessionId`

--- crates/collab2/src/lib.rs
+++ crates/collab2/src/lib.rs
@@ -3,6 +3,7 @@
 mod actions;
 mod collab;
 mod session;
+mod session_id;
 mod session_status;
 
 pub use collab::Collab;
---
+++ crates/collab2/src/session_id.rs
@@ -0,0 +1,37 @@
+use core::num::ParseIntError;
+use core::{fmt, str};
+
+use nomad::diagnostics::DiagnosticMessage;
+use nomad::CommandArgs;
+
+#[derive(Copy, Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub(crate) struct SessionId(pub(crate) collab_server::SessionId);
+
+impl fmt::Display for SessionId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:x}", self.0.into_u64())
+    }
+}
+
+impl str::FromStr for SessionId {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        u64::from_str_radix(s, 16).map(collab_server::SessionId::new).map(Self)
+    }
+}
+
+impl TryFrom<&mut CommandArgs> for SessionId {
+    type Error = DiagnosticMessage;
+
+    fn try_from(args: &mut CommandArgs) -> Result<Self, Self::Error> {
+        let [id] = args.as_slice() else {
+            todo!();
+        };
+        id.parse::<Self>().map_err(|err| {
+            let mut msg = DiagnosticMessage::new();
+            msg.push_str(err.to_string());
+            msg
+        })
+    }
+}
