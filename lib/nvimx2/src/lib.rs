fd43db50376ee3fdda0a1840bdca84087d8677db
Riccardo Mazzarini <me@noib3.dev>
2024-12-25 17:25:19 +0800

    nvimx2: move stuff under `nvimx-core`

--- lib/nvimx2/src/lib.rs
+++ lib/nvimx2/src/lib.rs
@@ -1,32 +1,6 @@
 //! TODO: docs.
 
-mod action;
-mod async_ctx;
-mod backend;
-mod command;
-mod command_args;
-pub mod executor;
-mod function;
-mod maybe_result;
-mod module;
-mod module_api;
-mod neovim_ctx;
-mod plugin;
-mod plugin_api;
-mod shared;
-
-pub use action::{Action, ActionName};
-pub use async_ctx::AsyncCtx;
-pub use backend::Backend;
 #[cfg(feature = "__neovim")]
 pub use backend_neovim as neovim;
-pub use command::Command;
-pub use command_args::CommandArgs;
-pub use function::Function;
-pub use maybe_result::MaybeResult;
-pub use module::{Module, ModuleCtx, ModuleName};
-pub use module_api::ModuleApi;
-pub use neovim_ctx::NeovimCtx;
-pub use plugin::{Plugin, PluginCtx, PluginName};
-pub use plugin_api::PluginApi;
-pub use shared::Shared;
+#[doc(inline)]
+pub use nvimx_core::*;
