e2f819dc73f90fb6fe6a11543bbab5ba3deaa3aa
Riccardo Mazzarini <me@noib3.dev>
2025-02-01 18:08:42 +0800

    Implement `Buffer<B>` for `&mut Buf`

--- lib/nvimx2/nvimx-core/src/backend/buffer.rs
+++ lib/nvimx2/nvimx-core/src/backend/buffer.rs
@@ -14,3 +14,20 @@ pub trait Buffer<B: Backend> {
     /// TODO: docs.
     fn name(&self) -> Cow<'_, str>;
 }
+
+impl<Buf: Buffer<B>, B: Backend> Buffer<B> for &mut Buf {
+    #[inline]
+    fn byte_len(&self) -> ByteOffset {
+        Buf::byte_len(self)
+    }
+
+    #[inline]
+    fn id(&self) -> B::BufferId {
+        Buf::id(self)
+    }
+
+    #[inline]
+    fn name(&self) -> Cow<'_, str> {
+        Buf::name(self)
+    }
+}
