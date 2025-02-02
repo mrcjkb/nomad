361c1fb55cb68e5fb5b0d014353e594297aa8b56
Riccardo Mazzarini <me@noib3.dev>
2025-02-02 21:14:37 +0800

    Implement `Backend` for `TestBackend`

--- lib/nvimx2/backend-test/src/backend.rs
+++ lib/nvimx2/backend-test/src/backend.rs
@@ -1,2 +1,100 @@
+use fxhash::FxHashMap;
+use nvimx_core::backend::{ApiValue, Backend, BufferId};
+use nvimx_core::module::Module;
+use nvimx_core::notify::MaybeResult;
+use serde::{Deserialize, Serialize};
+
+use crate::buffer::{TestBuffer, TestBufferId};
+use crate::emitter::TestEmitter;
+use crate::executor::TestExecutor;
+use crate::fs::TestFs;
+use crate::serde::{TestDeserializeError, TestSerializeError};
+
 /// TODO: docs.
-pub struct TestBackend;
+pub struct TestBackend {
+    buffers: FxHashMap<TestBufferId, TestBuffer>,
+    current_buffer: Option<TestBufferId>,
+    emitter: TestEmitter,
+    executor: TestExecutor,
+    fs: TestFs,
+}
+
+impl TestBackend {
+    pub fn new(fs: TestFs) -> Self {
+        Self {
+            buffers: Default::default(),
+            current_buffer: None,
+            emitter: Default::default(),
+            executor: Default::default(),
+            fs,
+        }
+    }
+}
+
+impl Backend for TestBackend {
+    type Api = crate::api::TestApi;
+    type Buffer<'a> = &'a mut TestBuffer;
+    type BufferId = TestBufferId;
+    type LocalExecutor = TestExecutor;
+    type BackgroundExecutor = TestExecutor;
+    type Fs = TestFs;
+    type Emitter<'this> = &'this mut TestEmitter;
+    type SerializeError = TestSerializeError;
+    type DeserializeError = TestDeserializeError;
+
+    fn api<M: Module<Self>>(&mut self) -> Self::Api {
+        Default::default()
+    }
+
+    fn init() -> Self {
+        todo!()
+    }
+
+    fn buffer(&mut self, id: BufferId<Self>) -> Option<Self::Buffer<'_>> {
+        self.buffers.get_mut(&id)
+    }
+
+    fn buffer_ids(&mut self) -> impl Iterator<Item = BufferId<Self>> + use<> {
+        self.buffers.keys().copied().collect::<Vec<_>>().into_iter()
+    }
+
+    fn current_buffer(&mut self) -> Option<Self::Buffer<'_>> {
+        self.current_buffer.map(|id| self.buffer(id).expect("buffer exists"))
+    }
+
+    fn fs(&mut self) -> Self::Fs {
+        self.fs.clone()
+    }
+
+    fn emitter(&mut self) -> Self::Emitter<'_> {
+        &mut self.emitter
+    }
+
+    fn local_executor(&mut self) -> &mut Self::LocalExecutor {
+        &mut self.executor
+    }
+
+    fn background_executor(&mut self) -> &mut Self::BackgroundExecutor {
+        &mut self.executor
+    }
+
+    fn serialize<T>(
+        &mut self,
+        value: &T,
+    ) -> impl MaybeResult<ApiValue<Self>, Error = Self::SerializeError>
+    where
+        T: ?Sized + Serialize,
+    {
+        crate::serde::serialize(value)
+    }
+
+    fn deserialize<'de, T>(
+        &mut self,
+        value: ApiValue<Self>,
+    ) -> impl MaybeResult<T, Error = Self::DeserializeError>
+    where
+        T: Deserialize<'de>,
+    {
+        crate::serde::deserialize(value)
+    }
+}
