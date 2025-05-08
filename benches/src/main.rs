d7897b8ac1dd65bf30bedc06ea9452f127750fd2
Riccardo Mazzarini <me@noib3.dev>
2025-05-08 17:42:48 +0200

    benches: fix warnings when all features are turned off

--- benches/src/main.rs
+++ benches/src/main.rs
@@ -1,12 +1,11 @@
 #![allow(missing_docs)]
 
-use criterion::{Criterion, criterion_main};
-
 fn benches() {
-    let mut criterion = Criterion::default().configure_from_args();
+    #[cfg(any(feature = "collab"))]
+    let mut criterion = criterion::Criterion::default().configure_from_args();
 
     #[cfg(feature = "collab")]
     benches::collab(&mut criterion);
 }
 
-criterion_main!(benches);
+criterion::criterion_main!(benches);
