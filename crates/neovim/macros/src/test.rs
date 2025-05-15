2a597d5972742f5998838955a97effd97cbcf4ea
Riccardo Mazzarini <me@noib3.dev>
2025-05-15 19:00:00 +0200

    feat(neovim): support async tests

--- crates/neovim/macros/src/test.rs
+++ crates/neovim/macros/src/test.rs
@@ -23,31 +23,42 @@ pub(crate) fn test(
     let test_body = &item.block;
     let test_output = &item.sig.output;
 
-    let ctx_name = match item.sig.inputs.first().expect("just checked") {
-        syn::FnArg::Typed(arg) => match &*arg.pat {
-            syn::Pat::Ident(pat_ident) => &pat_ident.ident,
-            _ => {
-                return Err(syn::Error::new(
-                    arg.pat.span(),
-                    "expected a named function argument",
-                ));
-            },
-        },
-        syn::FnArg::Receiver(arg) => {
+    let ctx_arg = match item.sig.inputs.first().expect("just checked") {
+        syn::FnArg::Typed(arg) => arg,
+        syn::FnArg::Receiver(_) => {
             return Err(syn::Error::new(
-                arg.self_token.span,
+                item.sig.ident.span(),
                 "expected a named function argument, not self",
             ));
         },
     };
 
-    let ctx_ty = if asyncness.is_some() {
-        quote! { &mut ::ed::AsyncCtx<'_, ::neovim::Neovim> }
-    } else {
-        quote! { &mut ::ed::EditorCtx<'_, ::neovim::Neovim> }
+    let ctx_name = match &*ctx_arg.pat {
+        syn::Pat::Ident(pat_ident) => &pat_ident.ident,
+        other => {
+            return Err(syn::Error::new(
+                other.span(),
+                "expected a named function argument",
+            ));
+        },
     };
 
+    let ctx_ty = &ctx_arg.ty;
+
     let augroup_name = test_name.to_string();
+
+    let run_test = if asyncness.is_some() {
+        quote! {
+            ::ed::backend::Backend::with_ctx(neovim, |ctx| {
+                ctx.spawn_local_unprotected(inner)
+            })
+            .await
+        }
+    } else {
+        quote! {
+            ::ed::backend::Backend::with_ctx(neovim, inner)
+        }
+    };
 
     Ok(quote! {
         #[::neovim::oxi::test(nvim_oxi = ::neovim::oxi)]
@@ -57,7 +68,7 @@ pub(crate) fn test(
                 #test_body
             }
             let neovim = ::neovim::Neovim::init(#augroup_name);
-            ::ed::backend::Backend::with_ctx(neovim, inner)
+            #run_test
         }
     })
 }
