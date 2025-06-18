0c8a8270c48f7719f61c738407b18985a7ea6319
Riccardo Mazzarini <me@noib3.dev>
2025-06-18 14:18:31 +0200

    build(nix): extract out common bits

--- flake.nix
+++ flake.nix
@@ -33,55 +33,92 @@
           lib,
           ...
         }:
-        {
-          packages =
-            let
-              nightly-toolchain = inputs'.fenix.packages.fromToolchainFile {
-                file = ./rust-toolchain.toml;
-                sha256 = "sha256-SISBvV1h7Ajhs8g0pNezC1/KGA0hnXnApQ/5//STUbs=";
+        let
+          common = {
+            devShells = {
+              default = pkgs.mkShell { };
+            };
+            rust = {
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                # TODO: remove after publishing private crates.
+                outputHashes = {
+                  "abs-path-0.1.0" = lib.fakeHash;
+                  "cauchy-0.1.0" = lib.fakeHash;
+                  "codecs-0.0.9" = lib.fakeHash;
+                  "lazy-await-0.1.0" = lib.fakeHash;
+                  "nvim-oxi-0.6.0" = lib.fakeHash;
+                  "pando-0.1.0" = lib.fakeHash;
+                  "puff-0.1.0" = lib.fakeHash;
+                };
               };
-            in
-            {
-              neovim =
-                (pkgs.makeRustPlatform {
+              platform =
+                let
+                  nightly-toolchain = inputs'.fenix.packages.fromToolchainFile {
+                    file = ./rust-toolchain.toml;
+                    sha256 = "sha256-SISBvV1h7Ajhs8g0pNezC1/KGA0hnXnApQ/5//STUbs=";
+                  };
+                in
+                pkgs.makeRustPlatform {
                   cargo = nightly-toolchain;
                   rustc = nightly-toolchain;
-                }).buildRustPackage
-                  {
-                    pname = "mad-neovim";
-                    version = "0.1.0";
-                    src = ./.;
-                    cargoLock = {
-                      lockFile = ./Cargo.lock;
-                      # TODO: remove after publishing private crates.
-                      outputHashes = {
-                        "abs-path-0.1.0" = lib.fakeHash;
-                        "cauchy-0.1.0" = lib.fakeHash;
-                        "codecs-0.0.9" = lib.fakeHash;
-                        "lazy-await-0.1.0" = lib.fakeHash;
-                        "nvim-oxi-0.6.0" = lib.fakeHash;
-                        "pando-0.1.0" = lib.fakeHash;
-                        "puff-0.1.0" = lib.fakeHash;
-                      };
-                    };
-                    buildPhase = ''
+                };
+            };
+          };
+
+          neovim =
+            let
+              buildPlugin =
+                let
+                  pname = "mad-neovim";
+                  version = "0.1.0";
+                in
+                {
+                  isNightly,
+                  isRelease ? true,
+                }:
+                common.rust.platform.buildRustPackage {
+                  inherit pname version;
+                  inherit (common.rust) cargoLock;
+                  src = ./.;
+                  buildPhase =
+                    let
+                      nightlyFlag = lib.optionalString isNightly "--nightly";
+                      releaseFlag = lib.optionalString isRelease "--release";
+                    in
+                    ''
                       runHook preBuild
-                      cargo xtask build --release
+                      cargo xtask build ${nightlyFlag} ${releaseFlag}
                       runHook postBuild
                     '';
-                    installPhase = ''
-                      runHook preInstall
-                      mkdir -p $out
-                      cp -r lua $out/
-                      runHook postInstall
-                    '';
-                  };
-              neovim-nightly = { };
+                  installPhase = ''
+                    runHook preInstall
+                    mkdir -p $out
+                    cp -r lua $out/
+                    runHook postInstall
+                  '';
+                };
+            in
+            {
+              packages = {
+                zero-dot-eleven = buildPlugin { isNightly = false; };
+                nightly = buildPlugin { isNightly = true; };
+              };
+              devShells = {
+                zero-dot-eleven = pkgs.mkShell { };
+                nightly = pkgs.mkShell { };
+              };
             };
+        in
+        {
+          packages = {
+            neovim = neovim.packages.zero-dot-eleven;
+            neovim-nightly = neovim.packages.nightly;
+          };
           devShells = {
-            default = pkgs.mkShell { };
-            neovim = pkgs.mkShell { };
-            neovim-nightly = pkgs.mkShell { };
+            default = common.devShells.default;
+            neovim = neovim.devShells.zero-dot-eleven;
+            neovim-nightly = neovim.devShells.nightly;
           };
           formatter = pkgs.nixfmt-rfc-style;
         };
