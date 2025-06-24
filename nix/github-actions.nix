7e8ce1996e707932ac3be59ce490f53291d11b66
Riccardo Mazzarini <me@noib3.dev>
2025-06-24 19:30:55 +0200

    Un-Nixify CI (#15)

--- nix/github-actions.nix
+++ nix/github-actions.nix
@@ -5,13 +5,49 @@
 {
   perSystem =
     {
+      pkgs,
+      lib,
       inputs',
+      crane,
       ...
     }:
+    let
+      devShells = {
+        tests = {
+          packages = with crane.lib; [
+            cargo
+            rustc
+          ];
+          env = {
+            # Setting this will disable some tests that fail in headless
+            # environments like CI.
+            HEADLESS = "true";
+          };
+        };
+      };
+
+      mkDevShell =
+        devShell:
+        let
+          cleanedDevShell = builtins.removeAttrs devShell [ "packages" ];
+        in
+        pkgs.mkShell (
+          cleanedDevShell
+          // {
+            buildInputs = (crane.commonArgs.buildInputs or [ ]) ++ (devShell.buildInputs or [ ]);
+            nativeBuildInputs = (crane.commonArgs.nativeBuildInputs or [ ]) ++ (devShell.packages or [ ]);
+          }
+        );
+    in
     {
       apps.nix-develop-gha = {
         type = "app";
         program = "${inputs'.nix-develop-gha.packages.default}/bin/nix-develop-gha";
       };
+
+      devShells = lib.mapAttrs' (name: devShell: {
+        name = "gha-${name}";
+        value = mkDevShell devShell;
+      }) devShells;
     };
 }
