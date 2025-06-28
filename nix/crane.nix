351b48c3874a0d6e001aeaf61818faad70d0de7c
Riccardo Mazzarini <me@noib3.dev>
2025-06-29 00:51:06 +0200

    refactor: tweaks

--- nix/crane.nix
+++ nix/crane.nix
@@ -22,38 +22,34 @@
                 || (type == "symlink" && lib.hasInfix "lua/nomad" path);
             }
           );
+
+          depsArgs = {
+            inherit (rust) buildInputs nativeBuildInputs;
+            # Crane will emit a warning if there's no `workspace.package.name`
+            # set in the workspace's Cargo.lock, so add a `pname` here to
+            # silence that.
+            pname = "nomad";
+            src = craneLib.cleanCargoSource (craneLib.path ../.);
+            strictDeps = true;
+          };
         in
         {
           lib = craneLib;
 
-          commonArgs =
-            let
-              args = {
-                inherit (rust) buildInputs nativeBuildInputs;
-
-                # Crane will emit a warning if there's no
-                # `workspace.package.name` set in the workspace's Cargo.lock,
-                # so add a `pname` here to silence that.
-                pname = "nomad";
-
-                src = craneLib.cleanCargoSource (craneLib.path ../.);
-
-                strictDeps = true;
-
-                env = {
-                  # Crane will run all 'cargo' invocation with `--release` if
-                  # this is not unset.
-                  CARGO_PROFILE = "";
-                  # The .git directory is always removed from the flake's
-                  # source files, so set the latest commit's hash and timestamp
-                  # via environment variables or crates/version's build script
-                  # will fail.
-                  COMMIT_HASH = inputs.self.rev or (lib.removeSuffix "-dirty" inputs.self.dirtyRev);
-                  COMMIT_UNIX_TIMESTAMP = toString inputs.self.lastModified;
-                };
-              };
-            in
-            args // { cargoArtifacts = craneLib.buildDepsOnly args; };
+          commonArgs = depsArgs // {
+            cargoArtifacts = craneLib.buildDepsOnly depsArgs;
+            env = {
+              # Crane will run all 'cargo' invocation with `--release` if this
+              # is not unset.
+              CARGO_PROFILE = "";
+              # The .git directory is always removed from the flake's source
+              # files, so set the latest commit's hash and timestamp via
+              # environment variables or crates/version's build script will
+              # fail.
+              COMMIT_HASH = inputs.self.rev or (lib.removeSuffix "-dirty" inputs.self.dirtyRev);
+              COMMIT_UNIX_TIMESTAMP = toString inputs.self.lastModified;
+            };
+          };
         };
     };
 }
