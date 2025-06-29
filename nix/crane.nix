4a353d2ee027cbca94b2f20a54d8142c81bba70d
Riccardo Mazzarini <me@noib3.dev>
2025-06-29 20:04:02 +0200

    feat: add a `crane.overridePkgs` function to start supporting cross-compilation

--- nix/crane.nix
+++ nix/crane.nix
@@ -12,11 +12,12 @@
     {
       _module.args.crane =
         let
-          craneLib =
-            ((inputs.crane.mkLib pkgs).overrideToolchain (
-              pkgs:
+          mkCraneLib =
+            targetPkgs:
+            ((inputs.crane.mkLib targetPkgs).overrideToolchain (
+              p:
               let
-                toolchain = rust.mkToolchain pkgs;
+                toolchain = rust.mkToolchain p;
               in
               # No fucking clue why this is necessary, but not having it causes
               # `lib.getExe' toolchain "cargo"` in the common.xtask derivation
@@ -35,34 +36,45 @@
                     (prev.filterCargoSources path type)
                     || (lib.hasSuffix ".lua" (builtins.baseNameOf path))
                     || (type == "symlink" && lib.hasInfix "lua/nomad" path);
-
                 }
               );
 
-          depsArgs = {
-            inherit (common) buildInputs nativeBuildInputs;
-            pname = common.workspaceName;
-            src = craneLib.cleanCargoSource (craneLib.path ../.);
-            strictDeps = true;
+          mkCommonArgs =
+            targetPkgs:
+            let
+              craneLib = mkCraneLib targetPkgs;
+
+              depsArgs = {
+                inherit (common) nativeBuildInputs;
+                buildinputs = common.buildInputs;
+                # buildinputs = common.buildInputs targetPkgs;
+                pname = common.workspaceName;
+                src = craneLib.cleanCargoSource (craneLib.path ../.);
+                strictDeps = true;
+              };
+            in
+            depsArgs
+            // {
+              cargoArtifacts = craneLib.buildDepsOnly depsArgs;
+              env = {
+                # Crane will run all 'cargo' invocations with `--release` if
+                # this is not unset.
+                CARGO_PROFILE = "";
+                # The .git directory is always removed from the flake's source
+                # files, so set the latest commit's hash and timestamp via
+                # environment variables or crates/version's build script will
+                # fail.
+                COMMIT_HASH = inputs.self.rev or (lib.removeSuffix "-dirty" inputs.self.dirtyRev);
+                COMMIT_UNIX_TIMESTAMP = toString inputs.self.lastModified;
+              };
+            };
+
+          mkCrane = targetPkgs: {
+            lib = mkCraneLib targetPkgs;
+            commonArgs = mkCommonArgs targetPkgs;
+            overridePkgs = targetPkgs2: mkCrane targetPkgs2;
           };
         in
-        {
-          lib = craneLib;
-
-          commonArgs = depsArgs // {
-            cargoArtifacts = craneLib.buildDepsOnly depsArgs;
-            env = {
-              # Crane will run all 'cargo' invocation with `--release` if this
-              # is not unset.
-              CARGO_PROFILE = "";
-              # The .git directory is always removed from the flake's source
-              # files, so set the latest commit's hash and timestamp via
-              # environment variables or crates/version's build script will
-              # fail.
-              COMMIT_HASH = inputs.self.rev or (lib.removeSuffix "-dirty" inputs.self.dirtyRev);
-              COMMIT_UNIX_TIMESTAMP = toString inputs.self.lastModified;
-            };
-          };
-        };
+        mkCrane pkgs;
     };
 }
