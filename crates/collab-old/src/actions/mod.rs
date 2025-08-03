81bcb1e6f1ed9f24a5aa2bd981283d4a2655daf9
Riccardo Mazzarini <me@noib3.dev>
2025-08-03 14:16:12 +0200

    chore: remove `collab-old`

--- crates/collab-old/src/actions/mod.rs
+++
@@ -1,9 +0,0 @@
-mod join;
-mod start;
-mod user_busy_error;
-mod yank;
-
-pub(crate) use join::Join;
-pub(crate) use start::Start;
-use user_busy_error::UserBusyError;
-pub(crate) use yank::Yank;
--- crates/collab-old/src/config.rs
+++
@@ -1,9 +0,0 @@
-use crate::server_socket::ServerSocket;
-
-#[derive(Default, serde::Deserialize)]
-#[serde(default)]
-#[serde(deny_unknown_fields)]
-pub struct Config {
-    /// TODO: docs.
-    pub(crate) server_socket: ServerSocket,
-}
