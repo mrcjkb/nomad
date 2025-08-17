83ad844d4fef39fb43f62134e4b594b4eda73e74
Riccardo Mazzarini <me@noib3.dev>
2025-08-17 21:29:33 +0200

    feat: implement most of the new `GitIgnore`

--- crates/walkdir/src/gitignore2.rs
+++ crates/walkdir/src/gitignore2.rs
@@ -1,9 +1,12 @@
-use std::{io, process};
+use std::collections::VecDeque;
+use std::io::{self, Write};
+use std::process;
+use std::sync::{Arc, OnceLock};
 
 use abs_path::{AbsPath, AbsPathBuf};
-use ed::executor::BackgroundSpawner;
-use ed::fs::os;
-use ed::{Editor, fs};
+use ed::executor::{BackgroundSpawner, Task};
+use ed::fs::{self, os};
+use futures_util::{StreamExt, select_biased};
 
 use crate::Filter;
 
@@ -11,8 +14,11 @@ use crate::Filter;
 /// used by Git.
 #[derive(Clone)]
 pub struct GitIgnore {
-    /// A sender used to send messages to the background task.
-    message_tx: flume::Sender<Message>,
+    /// A sender used to send [`CheckRequest`]s to the background task.
+    request_tx: flume::Sender<CheckRequest>,
+
+    /// The exit status of the `git check-ignore` process, if it has exited.
+    exit_status: Arc<OnceLock<io::Result<process::ExitStatus>>>,
 }
 
 /// The type of error that can occur when creating the [`GitIgnore`] filter.
@@ -47,31 +53,84 @@ pub enum GitIgnoreFilterError {
         /// The repo's path.
         repo_path: AbsPathBuf,
     },
+
+    /// The `git check-ignore` process has exited.
+    #[display(
+        "the 'git check-ignore ..' process has exited{}",
+        _0.map_or(Default::default(), |status| format!(" with status {status}"))
+    )]
+    ProcessExited(Option<process::ExitStatus>),
 }
 
-enum Message {
-    /// A request to check if a path is ignored, together with a sender that
-    /// the background task can use to send the result back.
-    CheckIgnore {
-        node_path: AbsPathBuf,
-        result_tx: flume::Sender<Result<bool, GitIgnoreFilterError>>,
-    },
+/// A request to check if a path is ignored, together with a sender that the
+/// background task can use to send the result back.
+struct CheckRequest {
+    node_path: AbsPathBuf,
+    result_tx: flume::Sender<Result<bool, GitIgnoreFilterError>>,
+}
 
-    /// All instances of the [`GitIgnore`] filter have been dropped, so the
-    /// background task can be stopped.
-    Stop,
+enum TaskMessage {
+    /// Sent by the stdout task when a new line is read. The `bool` indicates
+    /// whether the path (which is not included in the message) is ignored.
+    IgnoreOk(bool),
+
+    /// Send by the stderr task when an error occurs.
+    IgnoreError(GitIgnoreFilterError),
 }
 
 impl GitIgnore {
     /// Creates a new `GitIgnore` filter.
-    pub async fn new<Ed>(
-        _repo_path: AbsPathBuf,
-        _bg_spawner: &mut impl BackgroundSpawner,
-    ) -> Result<Self, GitIgnoreCreateError>
-    where
-        Ed: Editor<Fs = os::OsFs>,
-    {
-        todo!();
+    pub fn new(
+        repo_path: &AbsPath,
+        bg_spawner: &mut impl BackgroundSpawner,
+    ) -> Result<Self, GitIgnoreCreateError> {
+        let mut child = Self::command()
+            .current_dir(repo_path)
+            .stdin(process::Stdio::piped())
+            .stdout(process::Stdio::piped())
+            .stderr(process::Stdio::piped())
+            .spawn()
+            .map_err(GitIgnoreCreateError::CommandFailed)?;
+
+        let stdin = child.stdin.take().expect("stdin handle present");
+        let stdout = child.stdout.take().expect("stdout handle present");
+        let stderr = child.stderr.take().expect("stderr handle present");
+
+        let exit_status = Arc::new(OnceLock::new());
+        let (request_tx, request_rx) = flume::unbounded();
+        let (task_message_tx, task_message_rx) = flume::unbounded();
+
+        bg_spawner
+            .spawn({
+                let exit_status = exit_status.clone();
+                async move {
+                    Self::event_loop(
+                        child,
+                        stdin,
+                        request_rx,
+                        task_message_rx,
+                        exit_status,
+                    )
+                    .await;
+                }
+            })
+            .detach();
+
+        bg_spawner
+            .spawn({
+                let task_message_tx = task_message_tx.clone();
+                async move { Self::read_from_stdout(stdout, task_message_tx) }
+            })
+            .detach();
+
+        bg_spawner
+            .spawn({
+                let task_message_tx = task_message_tx.clone();
+                async move { Self::read_from_stderr(stderr, task_message_tx) }
+            })
+            .detach();
+
+        Ok(Self { request_tx, exit_status })
     }
 
     fn command() -> process::Command {
@@ -87,6 +146,88 @@ impl GitIgnore {
 
         cmd
     }
+
+    async fn event_loop(
+        mut child: process::Child,
+        mut stdin: process::ChildStdin,
+        request_rx: flume::Receiver<CheckRequest>,
+        task_message_rx: flume::Receiver<TaskMessage>,
+        exit_status: Arc<OnceLock<io::Result<process::ExitStatus>>>,
+    ) {
+        let mut request_stream = request_rx.into_stream();
+        let mut task_message_stream = task_message_rx.into_stream();
+        let mut result_tx_queue = VecDeque::new();
+
+        loop {
+            select_biased! {
+                request = request_stream.select_next_some() => {
+                    let path = request.node_path.as_str().as_bytes();
+
+                    let write_res = stdin
+                        .write_all(path)
+                        .and_then(|()| stdin.write_all(b"\0"));
+
+                    match write_res {
+                        Ok(()) => {
+                            result_tx_queue.push_front(request.result_tx)
+                        },
+                        Err(_) => {
+                            // Just give up if we can't write to stdin.
+                            break
+                        },
+                    }
+                },
+                task_message = task_message_stream.select_next_some() => {
+                    // We can always pop from the front of the queue because
+                    // 'git check-ignore' outputs paths in the same order they
+                    // were sent to stdin.
+                    let result_tx = result_tx_queue
+                        .pop_back()
+                        .expect("the queue should not be empty");
+
+                    let result = match task_message {
+                        TaskMessage::IgnoreOk(is_ignored) => Ok(is_ignored),
+                        TaskMessage::IgnoreError(err) => Err(err),
+                    };
+
+                    // The receiver might've been dropped, and that's ok.
+                    let _ = result_tx.send(result);
+                },
+                complete => break,
+            }
+        }
+
+        drop(stdin);
+
+        match exit_status.set(child.wait()) {
+            Ok(()) => (),
+            Err(_) => unreachable!("exit status only set once"),
+        }
+
+        for result_tx in result_tx_queue {
+            let _ = result_tx.send(Err(GitIgnoreFilterError::ProcessExited(
+                exit_status.get().expect("just set it").as_ref().ok().copied(),
+            )));
+        }
+    }
+
+    /// Continuosly reads from the `stdout` of the `git check-ignore` process
+    /// until it hits EOF or an error occurs.
+    fn read_from_stdout(
+        _stdout: process::ChildStdout,
+        _task_message_tx: flume::Sender<TaskMessage>,
+    ) {
+        todo!();
+    }
+
+    /// Continuosly reads from the `stderr` of the `git check-ignore` process
+    /// until it hits EOF or an error occurs.
+    fn read_from_stderr(
+        _stderr: process::ChildStderr,
+        _task_message_tx: flume::Sender<TaskMessage>,
+    ) {
+        todo!();
+    }
 }
 
 // We're shelling out to Git to get the list of ignored files, so this can only
@@ -99,23 +240,35 @@ impl Filter<os::OsFs> for GitIgnore {
         dir_path: &AbsPath,
         node_meta: &impl fs::Metadata<Fs = os::OsFs>,
     ) -> Result<bool, Self::Error> {
-        let node_name =
-            node_meta.name().map_err(GitIgnoreFilterError::NodeName)?;
+        loop {
+            if let Some(exit_status) = self.exit_status.get() {
+                return Err(GitIgnoreFilterError::ProcessExited(
+                    exit_status.as_ref().ok().cloned(),
+                ));
+            }
 
-        let (result_tx, result_rx) = flume::bounded(1);
+            let node_name =
+                node_meta.name().map_err(GitIgnoreFilterError::NodeName)?;
 
-        let message = Message::CheckIgnore {
-            node_path: dir_path.join(node_name),
-            result_tx,
-        };
+            let (result_tx, result_rx) = flume::bounded(1);
 
-        self.message_tx
-            .send(message)
-            .expect("background task hasn't been stopped");
+            let request = CheckRequest {
+                node_path: dir_path.join(node_name),
+                result_tx,
+            };
 
-        result_rx
-            .recv_async()
-            .await
-            .expect("background task hasn't been stopped")
+            if self.request_tx.send(request).is_err() {
+                // The background task just completed. Loop again, there will
+                // be an exit status set.
+                continue;
+            }
+
+            match result_rx.recv_async().await {
+                Ok(result) => return result,
+                // The background task just completed. Loop again, there will
+                // be an exit status set.
+                Err(_recv_err) => continue,
+            }
+        }
     }
 }
