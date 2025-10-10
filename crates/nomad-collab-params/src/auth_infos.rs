8dd316899f198546491f1aad571d7c52a4e15fb7
Riccardo Mazzarini <me@noib3.dev>
2025-10-10 03:30:42 +0200

    feat: add `AuthInfos::api_version`

--- crates/nomad-collab-params/src/auth_infos.rs
+++ crates/nomad-collab-params/src/auth_infos.rs
@@ -2,12 +2,15 @@
 /// server.
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct AuthInfos {
+    /// The API version the client is using.
+    pub api_version: u32,
+
     /// The JSON Web Token provided by the auth server.
     pub jwt: String,
 }
 
 impl From<auth_types::JsonWebToken> for AuthInfos {
     fn from(jwt: auth_types::JsonWebToken) -> Self {
-        Self { jwt: jwt.as_str().to_owned() }
+        Self { api_version: crate::API_VERSION, jwt: jwt.as_str().to_owned() }
     }
 }
